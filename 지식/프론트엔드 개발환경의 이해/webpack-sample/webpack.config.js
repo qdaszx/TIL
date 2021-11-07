de06ee198339b5c4f74d0907db69583d3b9fe39c
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-11-07 20:33:54 +0900

    웹팩(Webpack) 최소설정으로 프론트엔드 개발 환경 만들기 - clean-webpack-plugin
    dist 폴더에 기존에 남아 있던 이미지 파일을 웹팩이 자동으로 삭제하도록 하는 플러그인
    
    htmlwebpackplugin 처럼 require하여 씀

--- 지식/프론트엔드 개발환경의 이해/webpack-sample/webpack.config.js
+++ 지식/프론트엔드 개발환경의 이해/webpack-sample/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -30,6 +31,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 }
